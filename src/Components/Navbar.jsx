ed695c0cc75ee84409e6c7fc8a674de737a59aea
thashreef <chthashreef22@gmail.com>
2025-01-07 21:42:21 +0530

    responsive navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import arrowDown from "../assets/arrowdown.svg";
+import ham from "../assets/ham.svg";
 import k from "../assets/k.svg";
 import s from "../assets/s.svg";
 import zy from "../assets/zy.svg";
 const Navbar = () => {
   return (
-    <div className="px-32 h-[80px]  min-w-[1140px] border-b flex items-center justify-between">
+    <div className="px-32 h-[80px] w-full border-b flex items-center justify-between max-sm:px-[12px]">
       <div className="flex gap-[40px]">
-        <div className="h-[32px] w-[105px] flex relative">
+        {/* logo */}
+        <div className="h-[32px] w-[105px] flex relative max-sm:w-[84.61px]">
           <div className="w-[42.305px] h-full absolute left-0">
             <img src={zy} alt="logo letter" className="w-full h-full" />
           </div>
@@ -18,7 +20,9 @@ const Navbar = () => {
             <img src={k} alt="logo letter" className="w-full h-full" />
           </div>
         </div>
-        <ul className="flex w-[404px] h-[32px] gap-[32px] font-inter text-base font-semibold text-navColor">
+
+        {/* list */}
+        <ul className="flex w-[404px] h-[32px] gap-[32px] font-inter text-base font-semibold text-navColor max-md:hidden">
           <li className="cursor-pointer flex items-center">Home</li>
           <li className="cursor-pointer flex items-center gap-[8px]">
             Products{" "}
@@ -31,12 +35,19 @@ const Navbar = () => {
           <li className="cursor-pointer flex items-center">Pricing</li>
         </ul>
       </div>
-      <div className="w-[40px] h-[40px] rounded-full cursor-pointer">
+      {/* Profile */}
+      <div className="w-[40px] h-[40px] rounded-full cursor-pointer max-md:hidden">
         <img
           src="https://s3-alpha-sig.figma.com/img/6c1f/e88a/3b9e8dfddf4a065581b04df49638ca9c?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eKT1PQW5MJjG1wmo-acEjMlJw39zheyFtgFnhzE5GDTWDLExEyz7fs5oYHrWA38s1AAryAEXLznOKkxZdjVVoE4PcaKHZ1EUtGVzmLNkNcVMwzAhy0HsVGTc6gT~1e~JX3zWcYEp6rp~D4SK-Fm0whw8RkYwwN7dJKKEQtE6qdcOO-IQEwYWEhEtXUzGNKI7T4uw6v83wTtImimrUMMcCzYyGZO1EKHAFqMZg1Ur2olVQcD9gp~z1tveuNRk6FAXNjIeqKCmWLzx9ZImzFbTMHMzkmt7vqWK7fQ4aRPBq9Dz98F1-4JfbnkTvNC-0A5QTYP8VzRHXQc~wRJ9HlNZcg__"
           alt="profile"
           className="rounded-full"
         />
+      </div>
+
+      {/* hamburger */}
+
+      <div className="w-[40px] h-[40px]">
+        <img src={ham} alt="ham-burger" className="w-full h-full" />
       </div>
     </div>
   );
